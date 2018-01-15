222fe02d6933d497bf7fa07b1df70b19b633858f
zainabsaeed <zainabsaeed786@gmail.com>
2018-01-15 17:31:25 +0500

    1st commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+document.querySelector('.icon').addEventListener('click', function () {
+
+  document.querySelector('.overlay').style.display = 'none';
+
+}, false);
+document.querySelector('.show').addEventListener('click', function () {
+
+  document.querySelector('.overlay').style.display = 'block';
+  
+}, false);
